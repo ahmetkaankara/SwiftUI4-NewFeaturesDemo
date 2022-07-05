65f5037fb667b8b50e6f4e250134344df9512209
ahmetkaankara <ahmetkaankara1907@gmail.com>
2022-07-05 12:15:50 +0300

    Initial Commit

---
+++ SwiftUI4Tests/SwiftUI4TestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI4TestsApp.swift
+//  SwiftUI4Tests
+//
+//  Created by Ahmet Kaan Kara on 5.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI4TestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
